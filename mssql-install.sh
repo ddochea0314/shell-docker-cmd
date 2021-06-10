cfc7a80317bbb73357bcfcb8ab0e937073afd2f5
ddochea <ddochea0314@gmail.com>
2021-06-10 12:37:06 +0900

    remove sudo

--- mssql-install.sh
+++ mssql-install.sh
@@ -2,8 +2,8 @@
 
 name=sql1
 
-sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=root123!@#" \
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=root123!@#" \
    -p 1433:1433 --name $name -h $name \
    -d mcr.microsoft.com/mssql/server:2019-latest
 
-sudo docker start $name
+docker start $name
